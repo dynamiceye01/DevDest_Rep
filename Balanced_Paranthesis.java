a2512e32c4772ae4e94df3825c98331549fc5493
dynamiceye01 <115204782+dynamiceye01@users.noreply.github.com>
2022-10-07 09:42:51 +0530

    Create Balanced_Paranthesis.java

---
+++ Balanced_Paranthesis.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public int solve(String A) {
+        int n=A.length();
+        Stack<Character> stack=new Stack<>();
+        for(int i=0;i<n;i++)
+        {
+            char c=A.charAt(i);
+            if(c=='(' || c=='{' || c=='[')
+                stack.push(c);
+           
+               else if(c==')' && !stack.isEmpty() && stack.peek()=='(')
+                    stack.pop();
+                else if(c=='}' && !stack.isEmpty() && stack.peek()=='{')
+                    stack.pop();
+                else  if(c==']' && !stack.isEmpty() && stack.peek()=='[')
+                    stack.pop();
+                else
+                    return 0;
+    
+        }
+        if(stack.isEmpty())
+            return 1;
+        else 
+            return 0;
+    }
+}
