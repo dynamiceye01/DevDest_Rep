723e17b98635e19c7c9d5cebb24605922864f9be
Devansh Kashyap <47043096+dkashyap23@users.noreply.github.com>
2024-10-13 02:57:33 +0530

    Update Balanced_Paranthesis.java

--- Balanced_Paranthesis.java
+++ Balanced_Paranthesis.java
@@ -24,4 +24,29 @@ public class Solution {
         else 
             return 0;
     }
+    public int solve3(String A) {
+        int n=A.length();
+        Stack<Character> stack=new Stack<>();
+        for(int i=0;i<n;i++)
+        {
+            char c=A.charAt(i);
+            if(c=='(' || c=='{' || c=='[')
+                stack.push(c);
+           
+               else if(c==')' && !stack.isEmpty() && stack.peek()=='(')
+                    stack.pop();
+                else if(c=='}' && !stack.isEmpty() && stack.peek()=='{')
+                    stack.pop();
+                else  if(c==']' && !stack.isEmpty() && stack.peek()=='[')
+                    stack.pop();
+                else
+                    return 0;
+    
+        }
+        System.out.println("Stack");
+        if(stack.isEmpty())
+            return 1;
+        else 
+            return 0;
+    }
 }
